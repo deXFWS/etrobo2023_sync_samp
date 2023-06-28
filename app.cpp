9edd5c74aebce30c90b7410e27c53f936d655a3d
杜 <t.mori.rg@hitachi-kenki.com>
2023-06-28 15:33:42 +0900

    sample03
    import sample03

--- app.cpp
+++ app.cpp
@@ -1,52 +1,28 @@
-#include "app.h"
-#include "util.h"
-
-#include "Motor.h"
-#include "Clock.h"
-
+#include "app.h" // <1>
+#include "Tracer.h" // <2>
+#include "Clock.h"  // <3>
 using namespace ev3api;
 
-/**
- * メインタスク
- */
-void main_task(intptr_t unused) {
+Tracer tracer;  // <4>
+Clock clock;    // <5>
 
-  Motor leftWheel(PORT_C); // <1>
-  Motor rightWheel(PORT_B);  // <2>
-  Clock clock; // <3>
-
-#ifndef MAKE_RASPIKE
-  const int8_t pwm = (Motor::PWM_MAX) / 6;
-#else
-  const int8_t pwm = 60;
-#endif
-
-  const uint32_t duration = 2000*1000; // <1>
-
-  init_f(__FILE__);
-  while(1) {
-    msg_f("Forwarding...", 1);
-    leftWheel.setPWM(pwm);
-    rightWheel.setPWM(pwm);
-    clock.sleep(duration); // <1>
-
-    msg_f("Backwarding...", 1);
-    leftWheel.setPWM(-pwm);
-    rightWheel.setPWM(-pwm);
-    clock.sleep(duration);
-
-    // 左ボタンを長押し、それを捕捉する
-    if (ev3_button_is_pressed(LEFT_BUTTON)) {
-      break;
-    }
-  }
-
-  msg_f("Stopped.", 1);
-  leftWheel.stop();
-  rightWheel.stop();
-  while(ev3_button_is_pressed(LEFT_BUTTON)) {
-    ;
-  }
-
+void tracer_task(intptr_t exinf) { // <1>
+  tracer.run(); // <2>
   ext_tsk();
 }
+
+void main_task(intptr_t unused) { // <1>
+  const uint32_t duration = 100*1000; // <2>
+
+  tracer.init(); // <3>
+  sta_cyc(TRACER_CYC); // <4>
+  
+  while (!ev3_button_is_pressed(LEFT_BUTTON)) { // <1>
+      clock.sleep(duration);   // <2>
+  }
+
+  stp_cyc(TRACER_CYC); // <3>
+  tracer.terminate(); // <4>
+  ext_tsk(); // <5>
+}
+
