78cdca592b0fe47cc3b2dfcd00b2130d7efe8ecc
kaede-kaede <135792117+kaede-kaede@users.noreply.github.com>
2023-07-18 19:30:12 +0900

    Update ColorSensor.cpp
    Update ColorSensor.cpp

--- app/ColorSensor.cpp
+++ app/ColorSensor.cpp
@@ -1,9 +1,40 @@
 #include "ColorSensor.h"
 
+// #ifdef robo_no,,,
 
-ColorSensor::ColorSensor():
+//定数宣言
+const float ColorSensor::BLACK_JDG = 10.0f;
+const float ColorSensor::WHITE_JDG = 50.0f;
+const float ColorSensor::BLUE_JDG = 50.0f;
+
+// #endif robo_no,,,
+
+/*
+コンストラクタ
+*/
+ColorSensor::ColorSensor(PORT_2)
+            :mColorSensor(colorSensor),
+             mBlack(10.0f),
+             mWhite(50.0f),
+             mBlue(50.0f){
+
+}
+
+/*
+カラーセンサ
+*/
+//ColorSensor::ColorSensor():
     //ColorSensor(PORT_2){}     //ポート番号確認
 
-    
+bool ColorSensor::ResBlack() const{
+    //反射光の強さを見て閾値以下で
+    if(mColorSensor.getBrightness)
+}
 
+/* 反射光は使用しないため無効化
+int8_t getBrightness() constz{
+    return mColorSensor.getBrightness(); //反射光の強さ(0-100)
+}
+*/
 
+void ColorSensor::getRAWColor()
